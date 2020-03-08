5eca361b12dd3b4d2c0279758f9bd98ad22539ce
Lawrence <lawrencesperez@gmail.com>
2020-03-08 15:32:24 +1100

    Updates

---
+++ precache-manifest.aa698072ccf6c4af7702c6e5af1ddc74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0884d15663c1d7d7cdf6f282d0aa8d1e",
+    "url": "/bfresorttennisclub/index.html"
+  },
+  {
+    "revision": "ad610c38a06a108338d2",
+    "url": "/bfresorttennisclub/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8ca3133543f322a66083",
+    "url": "/bfresorttennisclub/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad610c38a06a108338d2",
+    "url": "/bfresorttennisclub/static/js/2.b089a2f0.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/bfresorttennisclub/static/js/2.b089a2f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca3133543f322a66083",
+    "url": "/bfresorttennisclub/static/js/main.b84c4e27.chunk.js"
+  },
+  {
+    "revision": "83f0e0e57b4a207b7c0e",
+    "url": "/bfresorttennisclub/static/js/runtime-main.e0a63a21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bfresorttennisclub/static/media/logo.5d5d9eef.svg"
+  }
+]);
