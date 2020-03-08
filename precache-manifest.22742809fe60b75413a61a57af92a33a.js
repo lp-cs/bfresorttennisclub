d6fa55b3fb6701dab86092d12856cc8c3e397fed
Lawrence <lawrencesperez@gmail.com>
2020-03-08 15:53:43 +1100

    Updates

---
+++ precache-manifest.22742809fe60b75413a61a57af92a33a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a284c8cd065921f0900fae8c49df93b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad610c38a06a108338d2",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3adb799b247dbdeaa0ab",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad610c38a06a108338d2",
+    "url": "/static/js/2.b089a2f0.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.b089a2f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3adb799b247dbdeaa0ab",
+    "url": "/static/js/main.cf9581e5.chunk.js"
+  },
+  {
+    "revision": "6c7a854601df094cd865",
+    "url": "/static/js/runtime-main.b1de3039.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
